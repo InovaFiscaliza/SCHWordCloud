c738ed40124f288175fade07db96aeda8be2bf14
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2025-05-02 09:27:41 -0300

    Formated imports.

--- src/schwordcloud/__init__.py
+++ src/schwordcloud/__init__.py
@@ -1,6 +1,3 @@
 from .config import load_config_file
-from .datamanager import fetch_annotation, fetch_sch_database
-from .websearch import GoogleSearch
 from .datamanager import DataManager
-
-
+from .websearch import GoogleSearch
